694c1996a774301cb5af4f87214d118cd908271c
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-01 13:42:40 -0400

    June 1, 2022 1:42 PM

--- Dapp/JS/interaction.js
+++ Dapp/JS/interaction.js
@@ -1,7 +1,7 @@
 const ABI = window.abi;
 const contractAddress = "0x000000000000000000000000000000000000000";
 
-//what is the ethereum zero address?   
+//what is the ethereum zero address in a string format?
 
 
 document.getElementById("Test").innerText = "Hello World!";
