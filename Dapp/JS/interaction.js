011798e77956358450b97e83d10b3073d2c5c9ed
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-01 13:42:13 -0400

    June 1, 2022 1:42 PM

--- Dapp/JS/interaction.js
+++ Dapp/JS/interaction.js
@@ -1,4 +1,4 @@
 const ABI = window.abi;
-const contractAddress = "0x00";
+const contractAddress = "0x000000000000000000000000000000000000000";
 
 document.getElementById("Test").innerText = "Hello World!";
