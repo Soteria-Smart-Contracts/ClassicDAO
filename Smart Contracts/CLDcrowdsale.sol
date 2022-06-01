55be52cae348f0257bd3b7e89a2ce6586fca084f
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-01 15:54:16 -0400

    June 1, 2022 3:54 PM

--- Smart Contracts/CLDcrowdsale.sol
+++ Smart Contracts/CLDcrowdsale.sol
@@ -18,8 +18,8 @@ contract CLD_Crowdsale {
     address payable LiquidityAddress = payable(0x8dC5816556772F072967160E8d042c6D3e23B3AD); //This address will be used to add the 60% of crowdsale funds as liquidity for wETC-CLS
     address payable TreasuryFund = payable(0x104ce9b743e0D97f09e9c5D59a2A66Fd15b13b4b); //This address will be used to add the 16% of crowdsale funds as liquidity for wETC-CLS
     
-    address payable Dev_1 = payable(0xF07531463fEa78eed4ee9C2739A5eF053992160F); //Payout Wallet to a party who worked on ClassicDAO (CryptoGaralo) 12% of the Crowdsale Funds
-    address payable Dev_2 = payable(0xc932b3a342658A2d3dF79E4661f29DfF6D7e93Ce); //Payout Wallet to a party who worked on ClassicDAO (Soteria-Smart-Contracts) 12% of the Crowdsale Funds   
+    address payable Dev_1 = payable(0x35626e8B7aE2b2D4ab324a542f9B66069D7D1526); //Payout Wallet to a party who worked on ClassicDAO (CryptoGaralo) 12% of the Crowdsale Funds
+    address payable Dev_2 = payable(0xc98Fe13Aa0FeC8836F8D3f9dc4712c284aA858cE); //Payout Wallet to a party who worked on ClassicDAO (Soteria-Smart-Contracts) 12% of the Crowdsale Funds   
 
     
     //Crowdsale Mode struct 
