638f9de74ae36a6decde9310a55f90583a7ab454
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-01 15:50:46 -0400

    June 1, 2022 3:50 PM

--- Smart Contracts/CLDcrowdsale.sol
+++ Smart Contracts/CLDcrowdsale.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.4;
 
-//CLD1 test
+//CLD1 test: 0x537a505a4CF9F0fC76752fb3995C25e55ec015C6
+//CLD
 
 contract CLD_Crowdsale {
     address payable CLD;
