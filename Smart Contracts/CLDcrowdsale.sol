0be8363d3581238f51b53bdeb2edc1999ceeefe6
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-02 12:15:16 -0400

    June 2, 2022 12:15 PM

--- Smart Contracts/CLDcrowdsale.sol
+++ Smart Contracts/CLDcrowdsale.sol
@@ -117,7 +117,7 @@ contract CLD_Crowdsale {
         require(Crowdsale_Mode.Sale_Mode == 1);
         require(Setup == 1);
         
-        Crowdsale_End_Unix = (block.timestamp + 300);
+        Crowdsale_End_Unix = (block.timestamp + 600);
         Crowdsale_Mode.Sale_Mode_Text = ("Sale is Open to buy CLS");
         Crowdsale_Mode.Sale_Mode = 2;
         
