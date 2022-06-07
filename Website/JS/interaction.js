5ed09d41171cee640ff17b32bce75c654105afa5
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-07 14:23:43 -0400

    June 7, 2022 2:23 PM

--- Website/JS/interaction.js
+++ Website/JS/interaction.js
@@ -1,7 +1,5 @@
 const ABI = window.abi;
 const contractAddress = "0x0000000000000000000000000000000000000000";
-
-const abi = window.abi2;
 let account;
 let contract;
 
@@ -12,7 +10,7 @@ async function loginWithEth(){
         await ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = new Web3(ethereum);
         accountarray = await web3.eth.getAccounts();
-        contract = new window.web3.eth.Contract(abi, contractAddress, window.web3);
+        contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
         document.getElementById('WalletB').innerText = "Connected";
     } else {
