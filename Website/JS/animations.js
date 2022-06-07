bc420bc867e6a67f4a24846522ffcbafd330bd23
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-07 13:54:02 -0400

    June 7, 2022 1:54 PM

--- Website/JS/animations.js
+++ Website/JS/animations.js
@@ -1,8 +1,8 @@
 
 function setActive(id) {
     document.getElementById('menuone').className = "Mactive"; 
-    document.getElementById(id).className = "Mactive"; 
-    document.getElementById(id).className = "Mactive"; 
+    document.getElementById('menutwo').className = "Mactive"; 
+    document.getElementById('menutre').className = "Mactive"; 
 
     document.getElementById(id).className = "Mactive"; 
 }
