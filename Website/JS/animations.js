0a1ea72eb4454276b07890943c4a7029922e48e9
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-09 09:05:09 -0400

    June 9, 2022 9:05 AM

--- Website/JS/animations.js
+++ Website/JS/animations.js
@@ -1,6 +1,6 @@
 let currentpage;
 
-document.getElementById('overlay').style.display = "none";
+document.getElementById('crowdsale').style.display = "none";
 
 if(window.location.href == "https://classicdao.one/dscrowdsale"){
     document.getElementById('menuone').className = "Mactive"; 
