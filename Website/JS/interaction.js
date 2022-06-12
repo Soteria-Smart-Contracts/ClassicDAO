419052f8a38e92c31cd206bf74742285703f5d52
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-12 12:46:50 -0400

    June 12, 2022 12:46 PM

--- Website/JS/interaction.js
+++ Website/JS/interaction.js
@@ -13,7 +13,7 @@ async function loginWithEth(){
         window.web3 = await new Web3(ethereum);
         await getID();
         console.log(netID);
-        if (netID != 5771){
+        if (netID != 5777){
             console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the ETC network.");
             alert("The current Metamask/Web3 network is not Ethereum Classic, please connect to the ETC network.");
             return("Failed to connect")
