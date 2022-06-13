0c9517b66669ab41b4872199e29ebab73865783e
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-13 13:33:24 -0400

    June 13, 2022 1:33 PM

--- Website/JS/interaction.js
+++ Website/JS/interaction.js
@@ -44,6 +44,6 @@ async function getMode(){
 }
 
 async function getBalance(){
-    let balance = await (web3.eth.getBalance(account));
+    let balance = await ((web3.eth.getBalance(account)));
 
 }
