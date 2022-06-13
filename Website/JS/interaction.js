38c33a41a124a323d9a62124e62173f6ab1acda8
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-13 14:09:38 -0400

    June 13, 2022 2:09 PM

--- Website/JS/interaction.js
+++ Website/JS/interaction.js
@@ -8,6 +8,12 @@ let netID;
 let mode;
 loginWithEth();
 
+accountInterval = setInterval(function() {
+    if (web3.eth.accounts[0] !== account) {
+      account = web3.eth.accounts[0];
+      updateInterface();
+    }
+  }, 100);
 
 
 async function loginWithEth(){
