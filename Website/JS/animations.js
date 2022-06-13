93b469453330a868c02f0044fc4477d75163aca3
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-13 15:25:53 -0400

    June 13, 2022 3:25 PM

--- Website/JS/animations.js
+++ Website/JS/animations.js
@@ -17,7 +17,7 @@ function setInterface(type){
     if (type == "none"){
         document.getElementById('f1').style.display = "initial";
         document.getElementById('f2').style.display = "initial";
-        document.getElementById('interface').style.backgroundColor = "initial";
+        document.getElementById('functionality').style.backgroundColor = "initial";
         document.getElementById('interface').onclick = null;
     }
     if (type == "deposit"){
