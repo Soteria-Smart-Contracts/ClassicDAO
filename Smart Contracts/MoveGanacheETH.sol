7fa504cad57997abe5ea16a391833e024b75f6f2
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-14 12:29:55 -0400

    June 14, 2022 12:29 PM

--- Smart Contracts/MoveGanacheETH.sol
+++ Smart Contracts/MoveGanacheETH.sol
@@ -3,16 +3,10 @@ pragma solidity ^0.8.4;
 
 
 contract MoveGanacheETH {
-    uint256 balance;
 
 
-    function MoveETH() public payable returns(uint256){
+    function MoveETH() public payable{
         payable(0x19c128C3ca3E853f3a093D946ba1c9De4Ac7b04F).transfer(msg.value);
-        return balance;
-    }
-
-    function getBalance(address tocheck) public view returns(uint256){
-        return(tocheck.balance);
     }
 }
 
