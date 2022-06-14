61783f9a8ac5216d8884a054f5d0524c859fb26a
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-14 14:20:56 -0400

    June 14, 2022 2:20 PM

--- Website/JS/animations.js
+++ Website/JS/animations.js
@@ -57,7 +57,7 @@ async function UpdateDetails(){
     document.getElementById('bal').innerText = balance;
     document.getElementById('dep').innerText = deposited;
     document.getElementById('totaldepo').innerText = totaldeposited;
-    document.getElementById('exrate').innerText = exchangeRate;
+    document.getElementById('exrate').innerText = exchangerate;
 
     if (mode[0] == 1){
     document.getElementById('salemode').innerText = 'Not Started';
