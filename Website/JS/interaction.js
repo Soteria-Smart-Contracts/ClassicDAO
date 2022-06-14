c654c5cf80a8e3fdd36a33ae7c0255bbe3f89e4b
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-06-14 14:12:38 -0400

    June 14, 2022 2:12 PM

--- Website/JS/interaction.js
+++ Website/JS/interaction.js
@@ -94,5 +94,5 @@ async function getETCDeposited(){
 async function getTotalDeposited(){
     let ftotal = await contract.methods.Total_ETC_Deposited().call();
     totaldeposited = (ftotal / 10**18).toFixed(2);
-    return(total);
+    return(totaldeposited);
 }
