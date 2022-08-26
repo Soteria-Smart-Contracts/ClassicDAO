ef3af2e94809a638d51492b4904736e395c0b5bf
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-08-26 12:11:25 +0000

    Aug 26, 2022, 8:11 AM

--- Website/JS/animations.js
+++ Website/JS/animations.js
@@ -7,7 +7,6 @@ document.body.style.overflow = "hidden";
 
 function setActive(id) {
     document.getElementById('menuone').className = "Minactive"; 
-    document.getElementById('menutwo').className = "Minactive"; 
     document.getElementById('menutre').className = "Minactive"; 
 
     document.getElementById(id).className = "Mactive"; 
