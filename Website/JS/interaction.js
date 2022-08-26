6e93ec6d4fc18f1996a4dedfd2d505773a89a801
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-08-26 11:53:00 +0000

    Aug 26, 2022, 7:53 AM

--- Website/JS/interaction.js
+++ Website/JS/interaction.js
@@ -32,7 +32,7 @@ async function loginWithEth(){
         contract = new window.web3.eth.Contract(ABI, contractAddress, window.web3);
         account = accountarray[0];
         if{
-            account 
+            
         }
         removeOverlay();
         UpdateDetails();
