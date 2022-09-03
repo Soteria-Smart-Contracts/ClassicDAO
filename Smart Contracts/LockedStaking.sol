39f7978cfd19a6469ff0c810067882affb2ef3e8
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-03 18:58:08 +0000

    Sep 3, 2022, 2:58 PM

--- Smart Contracts/LockedStaking.sol
+++ Smart Contracts/LockedStaking.sol
@@ -124,9 +124,9 @@ contract LockedStaking{
         uint256 len = UserLocks[User].length;
         uint256[] List;
         if(ActiveLocks[User] == 3){
-            List.push(len)
-            List.push(len - 1)
+            List.push(len -1)
             List.push(len - 2)
+            List.push(len - 3)
         }
         return()
     }
