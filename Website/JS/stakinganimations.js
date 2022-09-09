8d2c6097137d638e960772b054eee971c08eeebc
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-09 16:07:54 +0000

    Sep 9, 2022, 12:07 PM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -2,3 +2,8 @@ function showOverlay(){
     document.getElementById('overlay').style.display = "block";
     document.body.style.overflow = "hidden";
 }
+
+function removeOverlay(){
+    document.getElementById('overlay').style.display = "none";
+    document.body.style.overflow = "auto";
+}
