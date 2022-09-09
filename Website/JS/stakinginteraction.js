119addf615a5cb83a121b86349d46e4c6ce8713e
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-09 21:19:29 +0000

    Sep 9, 2022, 5:19 PM

--- Website/JS/stakinginteraction.js
+++ Website/JS/stakinginteraction.js
@@ -71,7 +71,7 @@ async function getCLDbal(){
 }
 
 async function getFlexDeposited(){
-    fbal = await CLDcontract.methods.balanceOf(account).call();
+    fbal = await CLDcontract.methods.Deposits(account).call();
 }
 
 
