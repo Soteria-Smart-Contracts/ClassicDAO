9e287481c083c93d935b3d293eee216fd3057221
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-09 16:11:23 +0000

    Sep 9, 2022, 12:11 PM

---
+++ Website/JS/stakinginteraction.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+
+
+
+let accountInterval = setInterval(function() {
+    if (web3.eth.accounts[0] !== account) {
+      account = web3.eth.accounts[0];
+      loginWithEth();
+    }
+  }, 300);
