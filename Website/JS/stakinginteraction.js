a6d3fbd35c97f73c37cc0c4639a281f4e50aa354
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-09 16:35:11 +0000

    Sep 9, 2022, 12:35 PM

--- Website/JS/stakinginteraction.js
+++ Website/JS/stakinginteraction.js
@@ -30,7 +30,7 @@ let accountInterval = setInterval(function() {
         }
         accountarray = await web3.eth.getAccounts();
         FlexContract = new window.web3.eth.Contract(flexABI, FlexContractAddress, window.web3);
-        CLDcontract = new window.web3.eth.Contract(CLDabi, FlexContractAddress, window.web3);
+        CLDcontract = new window.web3.eth.Contract(CLDabi, CLDcontractAddress, window.web3);
         account = accountarray[0];
  //       if(await FlexContract.methods.Eligibility(account).call() == false){
  //           alert("This address is not on the eligibility list for the ClassicDAO private sale. If you signed up for this sale but see this message, make sure you are using the correct wallet. If issues persist, please contact us on discord, twitter or telegram.")
