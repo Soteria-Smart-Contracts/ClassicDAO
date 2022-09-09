f94920f03c6d95415ccfde441d68a26648dfeeb0
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-09 21:28:53 +0000

    Sep 9, 2022, 5:28 PM

--- Website/JS/stakinginteraction.js
+++ Website/JS/stakinginteraction.js
@@ -50,7 +50,7 @@ let accountInterval = setInterval(function() {
 // let tx = await contract.methods.Buy().send({from: account, value: amountwei, gas: 300000});
 
 async function FlexDeposit{
-    
+    if()
 
 }
 
