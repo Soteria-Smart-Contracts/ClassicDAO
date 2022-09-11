b839ccbd6932f450b599afc076248218747fbc18
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-11 13:34:09 +0000

    Sep 11, 2022, 9:34 AM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -6,7 +6,7 @@ async function UpdateDetails(){
     await getUnclaimed();
     await getEstimated();
 
-    document.getElementById('flexbal').innerText = CLDbal;
+    document.getElementById('flexbal').innerText = (CLDbal;
     document.getElementById('flexdepo').innerText = FlexBal;
     document.getElementById('statdepo').innerText = FlexBal;
     document.getElementById('statunclaimed').innerText = Unclaimed;
