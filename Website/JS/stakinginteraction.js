acaf135fe90c26e2ac11fed9b005f41b59dffe2d
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-13 21:32:28 +0000

    Sep 13, 2022, 5:32 PM

--- Website/JS/stakinginteraction.js
+++ Website/JS/stakinginteraction.js
@@ -129,11 +129,7 @@ async function CreateLock(type){
     if(await CLDcontract.methods.allowance(account, LockContractAddress).call() < amount){
         await CLDcontract.methods.approve(FlexContractAddress, BigInt(1000000000000000000000000)).send({from: account, value: 0, gas: 300000});
     }
-    if(amount > CLDbal){
-        console.log(CLDbal)
-        alert('Your CLD balance is too low to stake this amount!')
-        return;
-    }
+    
     if(PreSaleUser = true){
         type = type + 1;
     }
