ecb13e90350512187937b71e177a37a218d62b64
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-13 21:41:35 +0000

    Sep 13, 2022, 5:41 PM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -16,7 +16,7 @@ async function UpdateDetails(){
     document.getElementById('statunclaimed').innerText = Unclaimed;
     document.getElementById('expected').innerText = Estimated;
 
-    document.getElementById('CR1').innerText = (((document.getElementById('One').value) / 100) * 102.5).toString();
+    document.getElementById('CR1').innerText = ((((document.getElementById('One').value) / 100) * 102.5)).toString();
     document.getElementById('CR2').innerText = (((document.getElementById('Two').value) / 100) * 108.75).toString();
     document.getElementById('CR3').innerText = (((document.getElementById('Three').value) / 100) * 120).toString();
     document.getElementById('CR4').innerText = (((document.getElementById('Four').value) / 100) * 150).toString();
