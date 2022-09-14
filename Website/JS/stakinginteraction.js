1e28082d1da044a28f902d304ba3218f4107f52b
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-14 15:49:29 +0000

    Sep 14, 2022, 11:49 AM

--- Website/JS/stakinginteraction.js
+++ Website/JS/stakinginteraction.js
@@ -120,7 +120,7 @@ async function LoadLocks(){
     }
     if(ActiveLockNum == 1){
         IDs.push(LocksLen - 1);
-        GetLock(1, ID);
+        GetLock(1, IDs[1]);
     }
     if(ActiveLockNum == 2){
         IDs = [(LocksLen - 1), (LocksLen - 2)]
