1de5cccbbf912a341e036987e035805921546216
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-14 17:58:59 +0000

    Sep 14, 2022, 1:58 PM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -69,7 +69,7 @@ async function DisplayInstance(number){
     document.getElementById('ViewLock').style.display = "initial";
     CurrentInstanceID = IDs[number];
 
-    i
+    if()
 
     dleft = await GetDaysLeft(number);
     amount = ((Locks[number].DepositAmount / 10**18).toFixed(2)).toString();
