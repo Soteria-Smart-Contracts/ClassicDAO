0b300e4b5effc6cd07ddbf792bc014b8e3c60d6d
Sebastien Ghert-Rousseau <scratchticketseb@gmail.com>
2022-09-14 16:25:27 +0000

    Sep 14, 2022, 12:25 PM

--- Website/JS/stakinginteraction.js
+++ Website/JS/stakinginteraction.js
@@ -137,7 +137,7 @@ async function LoadLocks(){
 
 async function GetDaysLeft(ID){
     dleft = await LockContract.methods.GetDaysLeft(Locks[ID][1], (Locks[ID][2]) - 1).call();
-    return(dleft);
+    return(dleft.promise);
 }
 
 async function GetLock(toset, id){
