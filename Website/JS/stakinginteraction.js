151e8bc920cd9cdea5dea44e29691ed57e774b2e
Darius Tanz <scratchticketseb@gmail.com>
2022-12-11 01:15:10 +0000

    Dec 10, 2022, 8:15 PM

--- Website/JS/stakinginteraction.js
+++ Website/JS/stakinginteraction.js
@@ -33,7 +33,7 @@ let accountInterval = setInterval(function() {
 
   async function loginWithEth(){
     if(window.ethereum){
-        if(LoggedIn = false){
+        if(LoggedIn = true){
             await ethereum.request({ method: 'eth_requestAccounts' });
             window.web3 = await new Web3(ethereum);
             LoggedIn = true;
