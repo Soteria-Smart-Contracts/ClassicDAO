e0941276e4cf959ebf266dd7268de247a9c12b50
Darius Tanz <scratchticketseb@gmail.com>
2022-12-21 15:49:42 +0000

    Dec 21, 2022, 10:49 AM

--- Website/JS/stakinginteraction.js
+++ Website/JS/stakinginteraction.js
@@ -130,7 +130,8 @@ async function LoadLocks(){
     while(ActiveLocksFound <= ActiveLockNum){
         LockCheck = await LockContract.methods.UserLocks(account, id).call();
         if(LockCheck[2] != 66){
-            Locks[CurrentToSet] = 
+            Locks[CurrentToSet] = LockCheck;
+            
         }
     }
 }
