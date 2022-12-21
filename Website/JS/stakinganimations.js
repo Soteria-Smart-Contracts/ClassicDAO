fd7c5691e347fab665a16f1ecc18b6fd80370536
Darius Tanz <scratchticketseb@gmail.com>
2022-12-21 16:01:08 +0000

    Dec 21, 2022, 11:01 AM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -78,7 +78,7 @@ async function DisplayInstance(number){
         return;
     }
     document.getElementById('ViewLock').style.display = "initial";
-    CurrentInstanceID = Locks[number];
+    CurrentInstanceID = Locks[number][0];
 
     dleft = await GetDaysLeft(number);
     amount = ((Locks[number].DepositAmount / 10**18).toFixed(2)).toString();
