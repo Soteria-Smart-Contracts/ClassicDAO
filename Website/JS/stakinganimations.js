2510e3f6b97054dafbc90347406103ef8d75d665
Darius Tanz <scratchticketseb@gmail.com>
2023-01-07 16:47:27 +0000

    Jan 7, 2023, 11:47 AM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -52,7 +52,9 @@ async function UpdateUnclaimed(){
 
 async function GetHENS(){
     HENSname = await hens.js.getNameOfOwner(account)
-    if(HENSname = '')
+    if(HENSname = ''){
+        HENSname = "Connected"
+    }
 
     console.log("If there is an error above, its normal and not a problem, try catch does not work for Metamask errors")
 }
