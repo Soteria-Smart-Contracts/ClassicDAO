67629d70f3fc6adb244db198337e3f3a208738d2
Darius Tanz <scratchticketseb@gmail.com>
2023-01-07 16:32:50 +0000

    Jan 7, 2023, 11:32 AM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -50,7 +50,9 @@ async function UpdateUnclaimed(){
 }
 
 async function GetHENS(){
-    try {await hens.js.getNameOfOwner(account)}
+    try {
+        await hens.js.getNameOfOwner(account)
+    }
         catch(err){console.log("No Account Found")}
 }
 
