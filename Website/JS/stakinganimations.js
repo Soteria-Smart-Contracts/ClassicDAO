8c9be83cbbd3aefd0afc701034a2cc2cad0e4bf1
Darius Tanz <scratchticketseb@gmail.com>
2023-01-07 16:42:24 +0000

    Jan 7, 2023, 11:42 AM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -53,9 +53,7 @@ async function UpdateUnclaimed(){
 async function GetHENS(){
     HENSname = "Connected"
     await hens.js.getNameOfOwner(account);
-    console.log("If there is an error above, its normal, try catch does not work for Metamask errors")
-    console.log("This error is simply because your account does not have a HENS registration")
-
+    console.log("If there is an error above, its normal, try catch does not work for Metamask errors, ")
 }
 
 function DiplayBoxLeft(type){
