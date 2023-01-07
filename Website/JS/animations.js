6fb38f035e6f6803220ad3fc91ec01448e28bdd3
Darius Tanz <scratchticketseb@gmail.com>
2023-01-07 16:15:37 +0000

    Jan 7, 2023, 11:15 AM

--- Website/JS/animations.js
+++ Website/JS/animations.js
@@ -46,7 +46,7 @@ function setInterface(type){
 async function UpdateDetails(){
     await getBalance();
     await GetCLDleft();
-    await hens.js.getNameOfOwner('')
+    await hens.js.getNameOfOwner(account)
 
     document.getElementById('totaldepo').innerText = totaldeposited;
     document.getElementById('bal').innerText = balance;
