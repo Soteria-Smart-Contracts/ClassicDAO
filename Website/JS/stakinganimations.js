5bd40e5ff5c4599df84e54414f0819c145cf01bd
Darius Tanz <scratchticketseb@gmail.com>
2023-01-07 16:41:36 +0000

    Jan 7, 2023, 11:41 AM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -54,6 +54,7 @@ async function GetHENS(){
     HENSname = "Connected"
     await hens.js.getNameOfOwner(account);
     console.log("If there is an error above, its normal, try catch does not work for Metamask errors")
+    console.log("If there is an error above, its normal, try catch does not work for Metamask errors")
 
 }
 
