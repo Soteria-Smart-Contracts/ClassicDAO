37ed81c80853a1ce31ae947428f2970fa69e7c60
Darius Tanz <scratchticketseb@gmail.com>
2023-01-13 18:18:11 +0000

    Jan 13, 2023, 1:18 PM

--- Website/JS/stakinganimations.js
+++ Website/JS/stakinganimations.js
@@ -21,7 +21,7 @@ async function UpdateDetails(){
 
     SetInstances();
 
-    if(ActiveLockNum == 3){
+    if(ActiveLockNum < 3){
         document.getElementById('Lock').style.display = "none";
     }
 }
