cd068a2dd4a4907e009bc325647198dffc9ff288
Darius Tanz <scratchticketseb@gmail.com>
2023-12-08 17:20:44 +0000

    Dec 8, 2023, 12:20 PM

---
+++ Website/JS/Signups.js
@@ -0,0 +1,32 @@
+async function loginWithEth(){
+    if(window.ethereum){
+        if(LoggedIn == false){
+            await ethereum.request({ method: 'eth_requestAccounts' });
+            this.web3 = await new Web3(ethereum);
+            LoggedIn = true;
+            FlexContract = new window.web3.eth.Contract(flexABI, FlexContractAddress, window.web3);
+            LockContract = new window.web3.eth.Contract(LockABI, LockContractAddress, window.web3);
+            CLDcontract = new window.web3.eth.Contract(CLDabi, CLDcontractAddress, window.web3);
+        }
+        await getID();                                                                                              
+        if (netID != 61){
+            console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic network."); //CHANGE FOR REAL CROWDSALE TO ETC
+            alert("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic network.");
+            showOverlay();
+            return("Failed to connect")
+        }
+        else{
+            removeOverlay();
+        }
+        accountarray = await web3.eth.getAccounts();
+        oldaccount = account;
+        account = accountarray[0];
+        if(oldaccount != account){
+            await UpdateDetails();
+        }
+        document.getElementById('WalletB').innerText = HENSname;
+        UpdateUnclaimed();
+    } else {
+        alert("No ETHER Wallet available")
+    }
+}
