f004e108f6370799f41e7766358b91099c7c8c1f
Darius Tanz <scratchticketseb@gmail.com>
2023-12-08 17:29:21 +0000

    Dec 8, 2023, 12:29 PM

--- Website/JS/Signups.js
+++ Website/JS/Signups.js
@@ -119,3 +119,12 @@ function removeOverlay(){
     document.getElementById('overlay').style.display = "none";
     document.body.style.overflow = "auto";
 }
+
+async function GetHENS(){
+    HENSname = await hens.js.getNameOfOwner(account)
+    if(HENSname == ''){
+        HENSname = "Connected"
+    }
+
+    console.log("If there is an error above, its normal and not a problem, try catch does not work for Metamask errors")
+}
