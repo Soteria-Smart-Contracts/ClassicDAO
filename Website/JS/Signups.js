1ea9950d3aeedac2bfe91bc3f7b08969d6c56fb6
Darius Tanz <scratchticketseb@gmail.com>
2023-12-08 17:21:46 +0000

    Dec 8, 2023, 12:21 PM

--- Website/JS/Signups.js
+++ Website/JS/Signups.js
@@ -1,5 +1,5 @@
-let SignupAddress = "0x7cb26f4DC1D293670c1B2eEC16942f1bF259416C";
-let 
+const SignupAddress = "0x7cb26f4DC1D293670c1B2eEC16942f1bF259416C";
+let SignupABI = 
 
 async function loginWithEth(){
     if(window.ethereum){
