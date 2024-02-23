3049e8b60561c0f2aa561db3c4d781e2a94c5d73
Darius Tanz <scratchticketseb@gmail.com>
2024-02-23 22:26:03 +0000

    Feb 23, 2024, 5:26 PM

--- DAOapp/JS/Web3base.js
+++ DAOapp/JS/Web3base.js
@@ -46,10 +46,5 @@ function displayAddress(){
 
 async function GetHENS(){
     HENSname = await hens.js.getNameOfOwner(account)
-    if(HENSname == ''){
-        HENSname = "Connected"
-    }
-    
-
     console.log("If there is an error above, its normal and not a problem, try catch does not work for Metamask errors")
 }
