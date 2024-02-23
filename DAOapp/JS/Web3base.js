d9bafa45e8ba9b1607e3c555b79c8b72d6690b26
Darius Tanz <scratchticketseb@gmail.com>
2024-02-23 21:58:09 +0000

    Feb 23, 2024, 4:58 PM

--- DAOapp/JS/Web3base.js
+++ DAOapp/JS/Web3base.js
@@ -11,7 +11,7 @@ async function loginWithEth(){
     if(window.ethereum){
         await ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = await new Web3(ethereum);
-        await getID();
+        //await getID();
         if (netID != 61){ //Change and fix
             console.log("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic."); 
             alert("The current Metamask/Web3 network is not Ethereum Classic, please connect to the Ethereum Classic network.");
