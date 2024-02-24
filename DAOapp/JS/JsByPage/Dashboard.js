91f8c57027e06fb0752cd878c68de78da48650d0
Darius Tanz <scratchticketseb@gmail.com>
2024-02-24 22:48:35 +0000

    Feb 24, 2024, 5:48 PM

--- DAOapp/JS/JsByPage/Dashboard.js
+++ DAOapp/JS/JsByPage/Dashboard.js
@@ -9,7 +9,7 @@ async function runCodeWhenLoggedIn() {
 }
 
 async function LoadDashboard() {
-    document.getElementById("HeaderProposalID").innerText = CurrentProposalInfo[2].ID;
+    document.getElementById("HeaderProposalID").innerText = CurrentProposalInfo[2].ProposalID;
 }
 
 runCodeWhenLoggedIn();
