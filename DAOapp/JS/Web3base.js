6b2ad95ef041a05eeb8f2bfe50a1c439d3fe17ff
Darius Tanz <scratchticketseb@gmail.com>
2024-02-24 22:17:17 +0000

    Feb 24, 2024, 5:17 PM

--- DAOapp/JS/Web3base.js
+++ DAOapp/JS/Web3base.js
@@ -39,7 +39,7 @@ async function getID(){
 async function LoadCurrentProposal(){
     let OngoingID = await DAOvoting.methods.CurrentOngoingVote().call();
 
-    OngoingID = (OngoingID.toString()).pa;
+    OngoingID = (OngoingID.toString()).parseInt();
 
     console.log(OngoingID);
 }
