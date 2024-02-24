3702ff3ce6c1c4ae83f608835d0acd1086585d70
Darius Tanz <scratchticketseb@gmail.com>
2024-02-24 22:06:44 +0000

    Feb 24, 2024, 5:06 PM

--- DAOapp/JS/ABIs.js
+++ DAOapp/JS/ABIs.js
@@ -487,4 +487,846 @@ window.CoreABI = [
 	}
 ];
 
-window.VotingABI = 
+window.VotingABI = [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "Proposer",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "ProposalID",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "Timestamp",
+				"type": "uint256"
+			}
+		],
+		"name": "InstanceCreated",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "NewAddress",
+				"type": "address"
+			}
+		],
+		"name": "NewDAOAddress",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "donator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "VotingInstance",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "amountDonated",
+				"type": "uint256"
+			}
+		],
+		"name": "ProposalIncentivized",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "Voter",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "TotalSent",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "IncentiveShare",
+				"type": "uint256"
+			}
+		],
+		"name": "TokensReturned",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "Voter",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "VotingInstance",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "option",
+				"type": "string"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "votesCasted",
+				"type": "uint256"
+			}
+		],
+		"name": "VoteCast",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "VotingInstance",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "bool",
+				"name": "Result",
+				"type": "bool"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint8",
+				"name": "Multi",
+				"type": "uint8"
+			}
+		],
+		"name": "VotingEnded",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "VotingInstance",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "StartTime",
+				"type": "uint256"
+			}
+		],
+		"name": "VotingStarted",
+		"type": "event"
+	},
+	{
+		"inputs": [],
+		"name": "ActiveInstances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "BeginNextVote",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "VotingInstance",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "BurnCut",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "CLDAddress",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "CLD",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "enum Winslow_Voting_V1.Vote",
+				"name": "VoteChoice",
+				"type": "uint8"
+			},
+			{
+				"internalType": "enum Winslow_Voting_V1.MultiOptions",
+				"name": "OptionChoice",
+				"type": "uint8"
+			}
+		],
+		"name": "CastMultiVote",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			},
+			{
+				"internalType": "enum Winslow_Voting_V1.Vote",
+				"name": "VoteChoice",
+				"type": "uint8"
+			}
+		],
+		"name": "CastVote",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "newAddr",
+				"type": "address"
+			}
+		],
+		"name": "ChangeDAO",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "newQuorum",
+				"type": "uint256"
+			}
+		],
+		"name": "ChangeQuorum",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "CurrentOngoingVote",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "DAO",
+		"outputs": [
+			{
+				"internalType": "address payable",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "ExecutorCut",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_VoteInstance",
+				"type": "uint256"
+			}
+		],
+		"name": "GetVoteResult",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "Result",
+				"type": "bool"
+			},
+			{
+				"internalType": "uint8",
+				"name": "Multi",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_VoteInstance",
+				"type": "uint256"
+			}
+		],
+		"name": "GetVotingInstance",
+		"outputs": [
+			{
+				"components": [
+					{
+						"internalType": "uint256",
+						"name": "ProposalID",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "VoteStarts",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "VoteEnds",
+						"type": "uint256"
+					},
+					{
+						"internalType": "enum Winslow_Voting_V1.VoteStatus",
+						"name": "Status",
+						"type": "uint8"
+					},
+					{
+						"internalType": "address[]",
+						"name": "Voters",
+						"type": "address[]"
+					},
+					{
+						"internalType": "uint256",
+						"name": "TotalCLDVoted",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint8",
+						"name": "MaxMulti",
+						"type": "uint8"
+					},
+					{
+						"internalType": "uint256",
+						"name": "YEAvotes",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "NAYvotes",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "TotalIncentive",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "IncentivePerVote",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "CLDtoIncentive",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "CLDToBurn",
+						"type": "uint256"
+					},
+					{
+						"internalType": "uint256",
+						"name": "CLDToExecutioner",
+						"type": "uint256"
+					}
+				],
+				"internalType": "struct Winslow_Voting_V1.VoteInstance",
+				"name": "Instance",
+				"type": "tuple"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "GetVotingQueue",
+		"outputs": [
+			{
+				"internalType": "uint256[]",
+				"name": "",
+				"type": "uint256[]"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "VotingInstance",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "IncentivizeProposal",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "ProposalID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint8",
+				"name": "MaxMulti",
+				"type": "uint8"
+			}
+		],
+		"name": "InitializeVoteInstance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "VoteInstanceID",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "MultiVotes",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "OptionOne",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "OptionTwo",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "OptionThree",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "OptionFour",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "OptionFive",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "OngoingVote",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "Quorum",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "ReturnAllVotedTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "VotingInstance",
+				"type": "uint256"
+			}
+		],
+		"name": "ReturnTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "NewExecCut",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "NewBurnCut",
+				"type": "uint256"
+			}
+		],
+		"name": "SetTaxAmount",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "success",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "UserUnreturnedVotes",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "UserUnreturnedVotesIndex",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "Version",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "VoterInfo",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "VotesLocked",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "CLDReturned",
+				"type": "bool"
+			},
+			{
+				"internalType": "bool",
+				"name": "Voted",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "VotingInstances",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "ProposalID",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "VoteStarts",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "VoteEnds",
+				"type": "uint256"
+			},
+			{
+				"internalType": "enum Winslow_Voting_V1.VoteStatus",
+				"name": "Status",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint256",
+				"name": "TotalCLDVoted",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint8",
+				"name": "MaxMulti",
+				"type": "uint8"
+			},
+			{
+				"internalType": "uint256",
+				"name": "YEAvotes",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "NAYvotes",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "TotalIncentive",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "IncentivePerVote",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "CLDtoIncentive",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "CLDToBurn",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "CLDToExecutioner",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "VotingQueue",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "VotingQueueIndex",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
