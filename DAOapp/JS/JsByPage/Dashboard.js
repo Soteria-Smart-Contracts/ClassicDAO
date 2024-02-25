d93dc89fce6728ff83a64ac0738632851e553c5c
Darius Tanz <scratchticketseb@gmail.com>
2024-02-25 00:49:01 +0000

    Feb 24, 2024, 7:49 PM

--- DAOapp/JS/JsByPage/Dashboard.js
+++ DAOapp/JS/JsByPage/Dashboard.js
@@ -15,7 +15,7 @@ async function LoadDashboard() {
     //HeaderMemo found as memo in 2nd array of CurrentProposalInfo
     document.getElementById("HeaderMemo").innerText = CurrentProposalInfo[1].Memo;
     //HeaderTimeLeft id found in 3rd array of CurrentProposalInfo as VoteEnds in unix time, convert to format as such (2 days 6 hours)
-    
+    document.getElementById("HeaderTimeLeft").innerText = timeLeft(CurrentProposalInfo[2].VoteEnds);
 }
 
 runCodeWhenLoggedIn();
