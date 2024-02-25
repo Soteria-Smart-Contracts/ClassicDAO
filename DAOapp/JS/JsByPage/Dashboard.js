6c9784bab8d9e6e4e7feb4644b8e8bfdb9ae76b4
Darius Tanz <scratchticketseb@gmail.com>
2024-02-25 00:49:21 +0000

    Feb 24, 2024, 7:49 PM

--- DAOapp/JS/JsByPage/Dashboard.js
+++ DAOapp/JS/JsByPage/Dashboard.js
@@ -1,3 +1,4 @@
+import { timeLeft } from './path/to/timeLeft'; // Add the import statement for the timeLeft function
 async function runCodeWhenLoggedIn() {
     while (!LoggedIn) {
         await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
@@ -15,7 +16,7 @@ async function LoadDashboard() {
     //HeaderMemo found as memo in 2nd array of CurrentProposalInfo
     document.getElementById("HeaderMemo").innerText = CurrentProposalInfo[1].Memo;
     //HeaderTimeLeft id found in 3rd array of CurrentProposalInfo as VoteEnds in unix time, convert to format as such (2 days 6 hours)
-    document.getElementById("HeaderTimeLeft").innerText = timeLeft(CurrentProposalInfo[2].VoteEnds);
+    document.getElementById("HeaderTimeLeft").innerText = ;
 }
 
 runCodeWhenLoggedIn();
