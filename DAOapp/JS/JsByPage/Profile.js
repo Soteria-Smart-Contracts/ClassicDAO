4335d443f395b98d30f9e925e9985d9fba7110fd
Darius Tanz <scratchticketseb@gmail.com>
2024-02-27 21:06:22 +0000

    Feb 27, 2024, 4:06 PM

---
+++ DAOapp/JS/JsByPage/Profile.js
@@ -0,0 +1,11 @@
+// create a filesystem on top of Helia, in this case it's UnixFS
+const fs = unixfs(helia)
+
+// we will use this TextEncoder to turn strings into Uint8Arrays
+const encoder = new TextEncoder()
+const bytes = encoder.encode('Hello World 101')
+
+// add the bytes to your node and receive a unique content identifier
+const cid = await fs.addBytes(bytes)
+
+console.log('Added file:', cid.toString())
