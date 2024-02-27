7a74e1d508c5503188b78d522f3f6a33286fcefb
Darius Tanz <scratchticketseb@gmail.com>
2024-02-27 21:34:39 +0000

    Feb 27, 2024, 4:34 PM

---
+++ DAOapp/JS/JsByPage/Profile.js
@@ -0,0 +1,9 @@
+
+async function addFileToNode() {
+    const node = await Ipfs.create()
+  const results = await node.add('=^.^= meow meow')
+  const cid = results[0].hash
+  console.log('CID created via ipfs.add:', cid)
+  const data = await node.cat(cid)
+  console.log('Data read back via ipfs.cat:', new TextDecoder().decode(data))
+}
