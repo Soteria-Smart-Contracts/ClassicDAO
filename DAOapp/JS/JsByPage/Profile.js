3680d421335b2b6e0d263df3726f184e38e492c6
Darius Tanz <scratchticketseb@gmail.com>
2024-02-27 21:35:55 +0000

    Feb 27, 2024, 4:35 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -1,7 +1,7 @@
 
 async function addFileToNode() {
     const node = await Ipfs.create()
-    const results = await node.add('=^.^= meow meow')
+    const results = await node.add('ClassicDAO is the best DAO')
     const cid = results[0].hash
     console.log('CID created via ipfs.add:', cid)
     const data = await node.cat(cid)
