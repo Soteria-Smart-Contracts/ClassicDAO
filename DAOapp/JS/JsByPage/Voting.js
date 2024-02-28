4a4047402ce8d89824581f1612b17b0a78278f49
Darius Tanz <scratchticketseb@gmail.com>
2024-02-28 23:11:55 +0000

    Feb 28, 2024, 6:11 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -11,9 +11,9 @@ async function runCodeWhenLoggedIn() {
 async function LoadDashboard() {
     document.getElementById("id").innerText = (CurrentProposalInfo[2].ProposalID).toString();
     //HeaderProposer id
-    document.getElementById("HeaderProposer").innerText = (CurrentProposalInfo[0].Proposer).substring(0, 7) + '...' + (CurrentProposalInfo[0].Proposer).substring(account.length - 3, CurrentProposalInfo[0].Proposer);
+    document.getElementById("proposer").innerText = (CurrentProposalInfo[0].Proposer).substring(0, 7) + '...' + (CurrentProposalInfo[0].Proposer).substring(account.length - 3, CurrentProposalInfo[0].Proposer);
     //HeaderMemo found as memo in 2nd array of CurrentProposalInfo
-    document.getElementById("HeaderMemo").innerText = CurrentProposalInfo[1].Memo;
+    document.getElementById("ProposalMemo").innerText = CurrentProposalInfo[1].Memo;
     //HeaderTimeLeft id found in 3rd array of CurrentProposalInfo as VoteEnds in unix time, convert to format as such (2 days 6 hours)
     document.getElementById("HeaderTimeLeft").innerText = timeLeft(CurrentProposalInfo[2].VoteEnds);
 }
