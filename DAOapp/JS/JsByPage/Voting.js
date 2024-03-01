a77154455e2e306fd7ad572b23270537e5c24474
Darius Tanz <scratchticketseb@gmail.com>
2024-03-01 15:45:17 +0000

    Mar 1, 2024, 10:45 AM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -13,8 +13,8 @@ async function LoadDashboard() {
     //HeaderProposer id
     document.getElementById("proposer").innerText = (CurrentProposalInfo[0].Proposer).substring(0, 7) + '...' + (CurrentProposalInfo[0].Proposer).substring(account.length - 3, CurrentProposalInfo[0].Proposer);
     //HeaderMemo found as memo in 2nd array of CurrentProposalInfo (under .Memo). the memo and the description are combined in the same field in the contract, so the the memo is the first part of the description, the description is the second part of the description. they are devided by a comma, so split the description by "//".
-    document.getElementById("memo").innerText = (CurrentProposalInfo[1].Memo).split("//")[0];
-    document.getElementById("description").innerText = (CurrentProposalInfo[1].Memo).split("//")[1];
+    document.getElementById("ProposalMemo").innerText = (CurrentProposalInfo[1].Memo).split("//")[0];
+    document.getElementById("desc").innerText = (CurrentProposalInfo[1].Memo).split("//")[1];
 
 
 
