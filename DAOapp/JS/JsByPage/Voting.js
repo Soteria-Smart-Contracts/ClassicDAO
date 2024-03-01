b88927b529041ed2debea290f377b2219b00235e
Darius Tanz <scratchticketseb@gmail.com>
2024-03-01 15:47:51 +0000

    Mar 1, 2024, 10:47 AM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -25,7 +25,7 @@ async function LoadDashboard() {
     document.getElementById("VoteEnds").innerText = ToDateAndTime(CurrentProposalInfo[2].VoteEnds);
 
     //there are yea and nay bars, the width of the bars are the percentage of the total votes, calculate the percentage of the total votes for each and set the width of the bars to that percentage. IDs are yeabar and naybar. do the calculations and the updating in the same line
-    //start by getting all yae votes and nay votes, convert them from bigint wei to float, then add them together to get the total votes
+    //start by getting all yae votes and nay votes, convert them from bigint wei to float using 
     
 
     //set the number of votes for and against in the yeavotes and nayvotes ids
