d4e8ee3677c4652b20b385e49b0938d288b4f58e
Darius Tanz <scratchticketseb@gmail.com>
2024-03-01 15:30:04 +0000

    Mar 1, 2024, 10:30 AM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -17,12 +17,10 @@ async function LoadDashboard() {
     //HeaderTimeLeft id found in 3rd array of CurrentProposalInfo as VoteEnds in unix time, convert to format as such (2 days 6 hours)
     document.getElementById("VoteEnds").innerText = ToDateAndTime(CurrentProposalInfo[2].VoteEnds);
 
-    //there are yea and nay bars, the width of the bars are the percentage of the total votes, calculate the percentage of the total votes for each and set the width of the bars to that percentage. IDs are yeabar and naybar. do the calculations and the updating in the sam
+    //there are yea and nay bars, the width of the bars are the percentage of the total votes, calculate the percentage of the total votes for each and set the width of the bars to that percentage. IDs are yeabar and naybar. do the calculations and the updating in the same line
     let totalVotes = parseInt(CurrentProposalInfo[2].VotesFor) + parseInt(CurrentProposalInfo[2].VotesAgainst);
-    let yeaPercent = (parseInt(CurrentProposalInfo[2].VotesFor) / totalVotes) * 100;
-    let nayPercent = (parseInt(CurrentProposalInfo[2].VotesAgainst) / totalVotes) * 100;
-    document.getElementById("yeabar").style.width = yeaPercent + "%";
-    document.getElementById("naybar").style.width = nayPercent + "%";
+    document.getElementById("yeabar").style.width = (parseInt(CurrentProposalInfo[2].VotesFor) / totalVotes) * 100 + "%";
+    
 }
 
 runCodeWhenLoggedIn();
