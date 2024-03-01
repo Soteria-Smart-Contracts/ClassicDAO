e4eec6c5bd946ca7f4d6839b4d4474064559855b
Darius Tanz <scratchticketseb@gmail.com>
2024-03-01 15:46:48 +0000

    Mar 1, 2024, 10:46 AM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -25,6 +25,7 @@ async function LoadDashboard() {
     document.getElementById("VoteEnds").innerText = ToDateAndTime(CurrentProposalInfo[2].VoteEnds);
 
     //there are yea and nay bars, the width of the bars are the percentage of the total votes, calculate the percentage of the total votes for each and set the width of the bars to that percentage. IDs are yeabar and naybar. do the calculations and the updating in the same line
+    //start by getting 
     let totalVotes = parseInt(CurrentProposalInfo[2].YEAvotes) + parseInt(CurrentProposalInfo[2].NAYvotes);
     console.log("Total Votes: " + totalVotes);
     document.getElementById("yeabar").style.width = (parseInt(CurrentProposalInfo[2].VotesFor) / totalVotes) * 100 + "%";
