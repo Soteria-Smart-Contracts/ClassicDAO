088da92f0e517a07a161c62c99dc5c0e2b5e19d4
Darius Tanz <scratchticketseb@gmail.com>
2024-03-01 17:33:06 +0000

    Mar 1, 2024, 12:33 PM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -5,6 +5,7 @@ setInterval(function() {
     var countDownDate = 1709335406 * 1000;
     var now = new Date().getTime();
     var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    document.getElementById("genesiscountdown").innerText = days + "d";
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    
+    document.getElementById("genesiscountdown").innerHTML = hours + " hours ";
 }
