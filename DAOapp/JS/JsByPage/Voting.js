cf039fc246b21dd540fdfbcc412a7464fe186c95
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 06:26:11 +0000

    Mar 2, 2024, 1:26 AM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -74,7 +74,7 @@ async function SetVoteChoiceDisplay(choice){
 
 //submit vote witht the castVote function in the voting contract, the input is the votechoice, 0 for yea, 1 for nay, amount speficfied in id amountInput which you may need to convert to wei
 async function SubmitVote(){
-    //require the am
+    //require the amountInput id to be greater than 0.01 
     let gasEstimate = await DAOvoting.methods.castVote(CurrentProposalInfo[2].ProposalID, votechoice, web3.utils.toWei(document.getElementById("amountInput").value, 'ether')).estimateGas({ from: account });
     await DAOvoting.methods.castVote(CurrentProposalInfo[2].ProposalID, votechoice, web3.utils.toWei(document.getElementById("amountInput").value, 'ether')).send({ from: account, gas: gasEstimate });
     location.reload();
