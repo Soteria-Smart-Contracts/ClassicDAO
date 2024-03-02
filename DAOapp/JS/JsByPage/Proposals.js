22091aa4ac246aeed912fc4ae0b16fe59e6659c5
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 06:03:34 +0000

    Mar 2, 2024, 1:03 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -5,7 +5,7 @@ setInterval(function() {
     var countDownDate = 1709381409 * 1000;
     var now = new Date().getTime();
 
-    //if the countdown is over, display Voting on id PropStatus
+    //if the countdown is over, display Voting on id PropStatus, and display Ongoing on 
     var distance = countDownDate - now;
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     
