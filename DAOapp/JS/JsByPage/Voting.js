b3755b46cfe9c4c496e13bfc8d83922b0591ce87
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 23:45:05 +0000

    Mar 2, 2024, 6:45 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -87,7 +87,7 @@ async function LoadDashboard() {
         userDisplay = await CheckHENS(VoterList[i].Voter);
         //set the link to the users blockscout page at https://etc.blockscout.com/address/USERADDRESS on a new tab
         span.innerHTML = "<a href='https://etc.blockscout.com/address/" + VoterList[i].Voter + "' style='color: black; text-decoration: none;'>" + userDisplay + "</a> | " + VoterList[i].Amount + " CLD";
-        span.
+        s
         div.appendChild(span);
         document.getElementById("VoterList").appendChild(div);
     }
