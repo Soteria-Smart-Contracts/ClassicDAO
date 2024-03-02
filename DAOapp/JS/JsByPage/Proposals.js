d2c80153035cb2a8d61d4eedca818014a2cca99d
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 00:35:18 +0000

    Mar 1, 2024, 7:35 PM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -2,7 +2,7 @@
 
 //set a countdown to genesiscountdown id, the unix start time is: 1709335406, in the format of X hours
 setInterval(function() {
-    var countDownDate = 1709335406 * 1000;
+    var countDownDate = 1709381409 * 1000;
     var now = new Date().getTime();
     var distance = countDownDate - now;
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
