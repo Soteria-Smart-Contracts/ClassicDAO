65c8181b97706e1f52ec9eb53987248813f1e630
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 04:12:28 +0000

    Mar 1, 2024, 11:12 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -42,7 +42,9 @@ async function LoadDashboard() {
     document.getElementById("nayvotes").innerText = NAYvotes.toFixed(2);
 }
 
-async function CheckApproveVotingCLD
+async function CheckApproveVotingCLDContitional{
+    
+}
 
 //
 
