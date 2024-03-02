113c9652bc096ccb854a1a5b37a663d70d3d2ad6
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 06:23:39 +0000

    Mar 2, 2024, 1:23 AM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -72,7 +72,7 @@ async function SetVoteChoiceDisplay(choice){
     votechoice = choice ? 0 : 1;
 }
 
-//submit vote witht the castVote function in the voting contract, the input is the votechoice, 0 for yea, 1 for nay
+//submit vote witht the castVote function in the voting contract, the input is the votechoice, 0 for yea, 1 for nay, amount speficfied in id
 
 
 //
