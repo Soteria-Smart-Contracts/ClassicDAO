462ef83e00aaf666a05f48d339a9eed514bcab66
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 22:02:39 +0000

    Mar 2, 2024, 5:02 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -49,6 +49,7 @@ async function LoadDashboard() {
     YEAvotes = parseFloat(web3.utils.fromWei(CurrentProposalInfo[2].YEAvotes, 'ether'));
     NAYvotes = parseFloat(web3.utils.fromWei(CurrentProposalInfo[2].NAYvotes, 'ether'));
     TotalVotes = YEAvotes + NAYvotes;
+    Quorum 
     document.getElementById("yeabar").style.width = (YEAvotes / TotalVotes) * 100 + "%";
     document.getElementById("naybar").style.width = (NAYvotes / TotalVotes) * 100 + "%";  
 
