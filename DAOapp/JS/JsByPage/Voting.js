32606ca9107a87b7f0a0ce4c7b6417a165fa1152
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 21:41:27 +0000

    Mar 2, 2024, 4:41 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -69,7 +69,7 @@ async function LoadDashboard() {
 async function CheckApproveVotingCLDContitional(){
     //see if the user has approved the voting contract, if they have not, approve the voting contract for 696969696969969696969696969696969669696 tokens
     let approved = parseInt(await CLDtoken.methods.allowance(account, VotingAddress).call());
-    
+
     if(approved == 0){
         gas = await CLDtoken.methods.approve(VotingAddress, BigInt(11579208923731619542357098500868790785326998466564056403945758400791312963993)).estimateGas({from: account});
         await CLDtoken.methods.approve(VotingAddress, BigInt(11579208923731619542357098500868790785326998466564056403945758400791312963993)).send({from: account, value: 0, gas: gas});
