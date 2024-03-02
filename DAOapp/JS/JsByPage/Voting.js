97a949d7d1eb15f4733e7b5add3db8f11e68fc3c
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 20:27:19 +0000

    Mar 2, 2024, 3:27 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -19,7 +19,7 @@ async function LoadDashboard() {
     document.getElementById("desc").innerText = (CurrentProposalInfo[1].Memo).split("//")[1];
 
     //calculate the total incentive per vote, the total incentive per vote is the total incentive divided by the total votes, total incentive is CLDtoIncetive on  current proposal info 3rd array, total votes is TotalCLDVoted in 3rd array of CurrentProposalInfo
-    TotalIncentive = 
+    TotalIncentive = parseFloat(web3.utils.fromWei(CurrentProposalInfo[2].CLDtoIncentive, 'ether'));
     TotalIncentivePerVote = parseFloat(web3.utils.fromWei(CurrentProposalInfo[2].CLDtoIncentive, 'ether')) / parseFloat(web3.utils.fromWei(CurrentProposalInfo[2].TotalCLDVoted, 'ether'));
     Uservotedbool = (await DAOvoting.methods.VoterInfo(CurrentProposalInfo[2].ProposalID, account).call()).VotesLocked > 0;
 
