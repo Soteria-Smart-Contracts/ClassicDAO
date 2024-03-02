db37e321a0daa186fe209cc0d7ebe849126a5ba6
Darius Tanz <scratchticketseb@gmail.com>
2024-03-02 22:41:30 +0000

    Mar 2, 2024, 5:41 PM

--- DAOapp/JS/Web3base.js
+++ DAOapp/JS/Web3base.js
@@ -98,6 +98,7 @@ async function GetHENS(){
 }
 
 async function CheckHENS(address){
+    AddressETCNS = address;
     AddressETCNS = await hens.js.getNameOfOwner(address);
     return AddressETCNS;
 }
