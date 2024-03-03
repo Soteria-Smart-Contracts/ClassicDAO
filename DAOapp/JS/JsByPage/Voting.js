b3979d25f750d29f98ae94ae61004a51a79865c7
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 22:29:37 +0000

    Mar 3, 2024, 5:29 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -100,7 +100,8 @@ async function LoadDashboard() {
     document.getElementById("rangeIncentive").max = Number(web3.utils.fromWei(CLDtokenholdings, 'ether')).toFixed(2) - 0.0001;
 }
 
-//create a load voter list function that will load the voter list, so that we only need to call the function to load the voter list, then anot
+//create a load voter list function that will load the voter list, so that we only need to call the function to load the voter list, then another fucntion to detect changes in the voter list, so that we can call the load voter list function when the voter list changes
+
 
 async function CheckApproveVotingCLDContitional(){
     //see if the user has approved the voting contract, if they have not, approve the voting contract for 696969696969969696969696969696969669696 tokens
