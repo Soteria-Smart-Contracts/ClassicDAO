9cb382782a88cc85978e20101c93ee2c9fc8781d
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 23:29:01 +0000

    Mar 3, 2024, 6:29 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -22,7 +22,8 @@ async function GetVotedProposals(){
         let Proposal = await DAOcore.methods.Proposals(VotedProposals[i]).call();
         let ProposalStatus = await DAOvoting.methods.VotingInstances(VotedProposals[i]).call();
         VoteEnds = ProposalInfo.VoteEnds;
-        //if the vote ends is lower than the current time, the proposal has ended, so set the status to Ended, and set the time left to 
+        //if the vote ends is lower than the current time, the proposal has ended, so set the status to Ended, and set the time left to Over
+        let
         TimeLeft = timeLeft(VoteEnds);
         //if vote ends is not 0, the proposal has either st
         let ProposalStatusText = "Active";
