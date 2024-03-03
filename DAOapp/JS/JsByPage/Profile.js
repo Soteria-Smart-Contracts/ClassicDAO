e7d56772df09aa45eaaef46c18547bc3ea2ee466
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 23:14:06 +0000

    Mar 3, 2024, 6:14 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -1,1 +1,1 @@
-//push a list of all the users voted proposals to the page by calling the UserUnreturnedVotes function on the voting contract,
+//push a list of all the users voted proposals to the page by calling the UserUnreturnedVotes function on the voting contract, then for each proposal ID call the Proposals function on the core contract to get the proposal details
