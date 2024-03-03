ef124baf0ef91ba05323d647372075c5080f9468
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 23:53:18 +0000

    Mar 3, 2024, 6:53 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -1,18 +1,4 @@
-//push a list of all the users voted proposals to the page by calling the UserUnreturnedVotes function on the voting contract, then for each proposal ID call the Proposals function on the core contract to get the proposal details and status
-//here is the html to push to the UserVotedProposalsList
-//<div style="display: flex; justify-content: space-between; align-items:center; padding: 16px 20px; border-bottom:1.81818px solid rgb(255, 255, 255); font-size:16px; color:rgb(255, 255, 255); box-sizing: border-box;">
-{/* <div style="flex: 1;">Create new des...</div>
-<div style="flex: 1;">3241345241234</div>
-<div style="flex: 1;">
-    <button style="width: auto; background-color: #16ECD3; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>Active</button>
-</div>
-<div style="flex: 1;">12/27 17:00 ~ 01/06 17:00</div>
-<div style="flex: 1; display: flex; justify-content: flex-end;">
-    <a href="CurrentVoting.html" style="text-decoration: none; color: inherit; outline: none;">
-    <button class="view_more_button">View more</button>             
-    </a>
-</div>
-</div */}
+
 async function runCodeWhenLoggedIn() {
     while (!LoggedIn) {
         await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
