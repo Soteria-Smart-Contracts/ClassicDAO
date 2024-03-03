c9d39edbd7e3021f28986adf7b02f38c4befbbb1
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 23:17:35 +0000

    Mar 3, 2024, 6:17 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -22,6 +22,7 @@ async function GetVotedProposals(){
         let Proposal = await DAOcore.methods.Proposals(VotedProposals[i]).call();
         let ProposalInfo = await DAOcore.methods.ProposalInfos(VotedProposals[i]).call();
         let ProposalStatus = await DAOvoting.methods.VotingInstances(VotedProposals[i]).call();
+        Proposal
         let ProposalStatusText = "Active";
         if(ProposalStatus[0] == 1){
             ProposalStatusText = "Ended";
