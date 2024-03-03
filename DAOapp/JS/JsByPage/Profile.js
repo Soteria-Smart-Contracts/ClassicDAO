39e488b258f151189c529e5fa06b3b5f941946b9
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 23:15:26 +0000

    Mar 3, 2024, 6:15 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -15,4 +15,32 @@
 </div */}
 
 async function GetVotedProposals(){
-    
+    let VotedProposals = await DAOvoting.methods.UserUnreturnedVotes(account).call();
+    let VotedProposalsList = document.getElementById("UserVotedProposalsList");
+    VotedProposalsList.innerHTML = "";
+    for(let i = 0; i < VotedProposals.length; i++){
+        let Proposal = await DAOcore.methods.Proposals(VotedProposals[i]).call();
+        let ProposalInfo = await DAOcore.methods.ProposalInfos(VotedProposals[i]).call();
+        let ProposalStatus = await DAOvoting.methods.VotingInstances(VotedProposals[i]).call();
+        let ProposalStatusText = "Active";
+        if(ProposalStatus[0] == 1){
+            ProposalStatusText = "Ended";
+        }
+        let ProposalHTML = `
+        <div style="display: flex; justify-content: space-between; align-items:center; padding: 16px 20px; border-bottom:1.81818px solid rgb(255, 255, 255); font-size:16px; color:rgb(255, 255, 255); box-sizing: border-box;">
+            <div style="flex: 1;">${Proposal[0]}</div>
+            <div style="flex: 1;">${VotedProposals[i]}</div>
+            <div style="flex: 1;">
+                <button style="width: auto; background-color: #16ECD3; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>${ProposalStatusText}</button>
+            </div>
+            <div style="flex: 1;">${ProposalInfo[0]} ~ ${ProposalInfo[1]}</div>
+            <div style="flex: 1; display: flex; justify-content: flex-end;">
+                <a href="CurrentVoting.html" style="text-decoration: none; color: inherit; outline: none;">
+                <button class="view_more_button">View more</button>             
+                </a>
+            </div>
+        </div>
+        `;
+        VotedProposalsList.innerHTML += ProposalHTML;
+    }
+}
