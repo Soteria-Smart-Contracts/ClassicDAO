e716a934e15094365c68bed80a91d5111c8be344
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 22:38:52 +0000

    Mar 3, 2024, 5:38 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -106,7 +106,7 @@ async function LoadVoterList() {
     document.getElementById("TotalVoters").innerText = Voters.length;
     document.getElementById("VoterList").innerHTML = "";
     var div = document.createElement("div");
-    div.innerHTML = '<h1 id="votebuttonstext" style="font-size: 30px; margin-top: 0px; margin-bottom: 0px; font-family: GTWalsheim, bold; color: #fff;">Voters</h1>';
+    div.innerHTML = '<h1 id="votebuttonstext" style="font-size: 30px; margin-top: 0px; margin-bottom: 0px; font-family: GTWalsheim, bold; color: #fff;">Voters</h1> <hr style="border: 1px solid #fff; margin: 0;">';
     document.getElementById("VoterList").appendChild(div);
 
 
