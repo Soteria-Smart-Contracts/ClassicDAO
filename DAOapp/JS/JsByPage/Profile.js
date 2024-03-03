2baccfd7ddff10dd7f08fcaf51823638ac2fc0a9
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 23:50:15 +0000

    Mar 3, 2024, 6:50 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -33,6 +33,7 @@ async function GetVotedProposals(){
     for(let i = 0; i < 10; i++){
             let Proposal = await DAOvoting.methods.UserUnreturnedVotes(account, i).call();
             VotedProposals.push(Proposal);
+            console.log(Proposal);
         }
 
     VotedProposalsList.innerHTML = "";
