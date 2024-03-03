57778805003a711f02dde904baa964cbbc60d2a6
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 23:14:56 +0000

    Mar 3, 2024, 6:14 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -1,7 +1,7 @@
 //push a list of all the users voted proposals to the page by calling the UserUnreturnedVotes function on the voting contract, then for each proposal ID call the Proposals function on the core contract to get the proposal details and status
 //here is the html to push to the UserVotedProposalsList
 //<div style="display: flex; justify-content: space-between; align-items:center; padding: 16px 20px; border-bottom:1.81818px solid rgb(255, 255, 255); font-size:16px; color:rgb(255, 255, 255); box-sizing: border-box;">
-//* <div style="flex: 1;">Create new des...</div>
+{/* <div style="flex: 1;">Create new des...</div>
 <div style="flex: 1;">3241345241234</div>
 <div style="flex: 1;">
     <button style="width: auto; background-color: #16ECD3; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>Active</button>
