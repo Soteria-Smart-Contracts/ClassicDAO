c58ae8764805037799ad2d2964394c7abfb5a88f
Darius Tanz <scratchticketseb@gmail.com>
2024-03-03 22:34:43 +0000

    Mar 3, 2024, 5:34 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -105,7 +105,7 @@ async function LoadVoterList() {
     Voters = (await DAOvoting.methods.GetVotingInstance(CurrentProposalInfo[2].ProposalID).call()).Voters;
     document.getElementById("TotalVoters").innerText = Voters.length;
     // add a h1 element with the text "Voters" to the voterlist id "<h1 id="votebuttonstext" style="font-size: 30px; margin-top: 0px; margin-bottom: 0px; font-family: GTWalsheim, bold; color: #fff;">Voters</h1>"
-    
+    document
 
 
     document.getElementById("VoterList").innerHTML = "";
