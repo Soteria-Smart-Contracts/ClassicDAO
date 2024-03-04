996b3af952cc7ee52a9511e808b9a006eb57db1f
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 15:35:29 +0000

    Mar 4, 2024, 10:35 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -25,29 +25,30 @@ async function LoadProposals() {
     let ProposalQueueList = document.getElementById("ProposalQueueList");
     ProposalQueueList.innerHTML = "";
     for (let i = 0; i < ProposalQueue.length; i++) {
-        let ProposalID = ProposalQueue[i];
-        let ProposalInfo = await DAOvoting.methods.ProposalInfo(ProposalID).call();
-        let VotingInstanceID = ProposalInfo.VotingInstanceID;
-        let VotingInstance = await DAOvoting.methods.VotingInstances(VotingInstanceID).call();
-        let Status = VotingInstance.Status;
-        let CLDtoIncentive = web3.utils.fromWei(VotingInstance.CLDtoIncentive, 'ether');
-        let ProposalType = ProposalInfo.ProposalType;
+        let VotingInstance = await DAOvoting.methods.VotingInstances(ProposalQueue[i]).call();
+        let ProposalInfo = await DAOvoting.methods.ProposalInfo(ProposalQueue[i]).call();
+        let Status = "Voting";
+        if (VotingInstance.Status == 1) {
+            Status = "Ended";
+        }
+        let ProposalType = "Simple";
+        if (ProposalInfo.ProposalType == 1) {
+            ProposalType = "Eros";
+        }
+        let Countdown = timeLeft(ProposalInfo.GenesisStart);
         let ProposalHTML = `
         <div style="display: flex; justify-content: space-between; align-items:center; padding: 16px 20px; border-bottom:1.81818px solid rgb(255, 255, 255); font-size:16px; color:rgb(255, 255, 255); box-sizing: border-box;">
-            <div style="flex: 1;">Proposal ${ProposalID}</div>
-            <div style="flex: 1;">${Status}</div>
-            <div style="flex: 1;">${ProposalType}</div>
-            <div style="flex: 1;">${VotingInstanceID}</div>
-            <div style="flex: 1;">${Status}</div>
-            <div style="flex: 1;">${CLDtoIncentive} CLD</div>
+            <div style="flex: 1;">${i}</div>
+            <div style="flex: 1;">${ProposalQueue[i]}</div>
+            <div style="flex: 1;">
+                <button style="width: auto; background-color: #2908df; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>${Status}</button>
+            </div>
+            <div style="flex: 1;">
+                <button style="width: auto; background-color: #ffffff; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>${ProposalType}</button>
+            </div>
+            <div id="genesiscountdown" style="flex: 1;">${Countdown}</div>
             <div style="flex: 1; display: flex; justify-content: flex-end;">
                 <a href="CurrentVote.html" style="text-decoration: none; color: inherit; outline: none;">
-                <button class="view_more_button" style="background-color: #16ECD3;">View more</button>             
-                </a>
-            </div>
-        </div>
-        `;
-        ProposalQueueList.innerHTML += ProposalHTML;
-    }
+                <button class="
 }
     
