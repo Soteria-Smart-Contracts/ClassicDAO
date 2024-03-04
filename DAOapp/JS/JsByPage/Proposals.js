51dab79d4e7ce2ba48b08a77f229a9ffa9154eb3
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 15:38:05 +0000

    Mar 4, 2024, 10:38 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -23,7 +23,7 @@
 async function LoadProposals() {
     let ProposalQueue = await DAOvoting.methods.GetVotingQueue().call();
     let ProposalQueueList = document.getElementById("ProposalQueueList");
-    //find the highest queue position by checking the CLDtoIncentive of the VotingInstance
+    //find the highest queue position by checking the CLDtoIncentive of the VotingInstance, and updat
     ProposalQueueList.innerHTML = "";
     for (let i = 0; i < ProposalQueue.length; i++) {
         let VotingInstance = await DAOvoting.methods.VotingInstances(ProposalQueue[i]).call();
