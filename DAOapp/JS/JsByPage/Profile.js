f31cbdd35301f92c6049fe840f0d320df6ab8596
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 23:56:41 +0000

    Mar 4, 2024, 6:56 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -71,7 +71,7 @@ async function GetVotedProposals(){
             //set the reward to 2 decimal places
             //get propossal info from VotingInstances
             VoteInQuestion = await DAOvoting.methods.VotingInstances(VotedProposals[i]).call();
-            Reward = (web3.utils.fromWei(CurrentProposalInfo[2].IncentivePerVote, 'ether') * VoterVotesLocked).toFixed(2) + " CLD";
+            Reward = (web3.utils.fromWei(VoteInQuestion.IncentivePerVote, 'ether') * VoterVotesLocked).toFixed(2) + " CLD";
             NextProposalStarted = "";
             AmountButton = "Claim";
             Onclick = "ClaimInstance(" + VotedProposals[i] + ")";
