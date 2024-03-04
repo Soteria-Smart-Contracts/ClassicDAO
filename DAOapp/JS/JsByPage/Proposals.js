f6abcfee053b90c5e18c95a19c9c508aeb4aeb53
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 15:22:08 +0000

    Mar 4, 2024, 10:22 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -19,4 +19,4 @@
 // });
 
 
-//Load the proposal queue and the proposal details for each proposal in the queue. Here are the details needed: ID, Status, ProposalInfo.ProposalType, VotingInstanceID, VotingInstance.
+//Load the proposal queue and the proposal details for each proposal in the queue. Here are the details needed: ID, Status, ProposalInfo.ProposalType, VotingInstanceID, VotingInstance.Staus
