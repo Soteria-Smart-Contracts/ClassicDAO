65163c285889f996d6803b86cfabecd8fe1baedb
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 15:17:45 +0000

    Mar 4, 2024, 10:17 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -1,20 +1,22 @@
 
 
-//set a countdown to genesiscountdown id, the unix start time is: 1709335406, in the format of X hours
-setInterval(function() {
-    var countDownDate = 1709381409 * 1000;
-    var now = new Date().getTime();
+// //set a countdown to genesiscountdown id, the unix start time is: 1709335406, in the format of X hours
+// setInterval(function() {
+//     var countDownDate = 1709381409 * 1000;
+//     var now = new Date().getTime();
 
-    //if the countdown is over, display Voting on id PropStatus, and display Ongoing on genesiscountdown id
-    if (now > countDownDate) {
-        document.getElementById("PropStatus").innerHTML = "Voting";
-        document.getElementById("genesiscountdown").innerHTML = "Ongoing";
-    }
-    else{
-        var distance = countDownDate - now;
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     //if the countdown is over, display Voting on id PropStatus, and display Ongoing on genesiscountdown id
+//     if (now > countDownDate) {
+//         document.getElementById("PropStatus").innerHTML = "Voting";
+//         document.getElementById("genesiscountdown").innerHTML = "Ongoing";
+//     }
+//     else{
+//         var distance = countDownDate - now;
+//         var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         
-        document.getElementById("genesiscountdown").innerHTML = "in " + hours + " hours ";
-    }
-});
+//         document.getElementById("genesiscountdown").innerHTML = "in " + hours + " hours ";
+//     }
+// });
 
+
+//Load all p
