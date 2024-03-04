d32457ba8e86e35e2ac15f2837a52515c3df261d
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 04:31:11 +0000

    Mar 3, 2024, 11:31 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -67,11 +67,11 @@ async function GetVotedProposals(){
         let ProposalHTML = `
         <div style="display: flex; justify-content: space-between; align-items:center; padding: 16px 20px; border-bottom:1.81818px solid rgb(255, 255, 255); font-size:16px; color:rgb(255, 255, 255); box-sizing: border-box;">
             <div style="flex: 1;">${VotedProposals[i]}</div>
-            <div style="flex: 1;">
+            <div style="flex: 2;">
                 <button style="width: auto; background-color: #16ECD3; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>${ProposalStatus}</button>
             </div>
-            <div style="flex: 1;">${TimeLeft}</div>
-            <div style="flex: 1; display: flex;">
+            <div style="flex: 3;">${TimeLeft}</div>
+            <div style="flex: 4; display: flex;">
                 <a id="${ID}" onclick="${Onclick}" style="text-decoration: none; color: inherit; outline: none;">
                 <button class="view_more_button">${AmountButton}</button>             
                 </a>
