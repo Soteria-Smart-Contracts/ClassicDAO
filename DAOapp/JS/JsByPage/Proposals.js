1b229a312074ec1b672ef6c5f463218f490c245a
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 16:00:14 +0000

    Mar 4, 2024, 11:00 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -56,22 +56,7 @@ async function LoadProposals() {
         }
         let ProposalHTML = `
         <div style="display: flex; justify-content: space-between; align-items:center; padding: 16px 20px; border-bottom:1.81818px solid rgb(255, 255, 255); font-size:16px; color:rgb(255, 255, 255); box-sizing: border-box;">
-            <div style="flex: 1;">${i}</div>
-            <div style="flex: 1;">${ProposalQueue[i]}</div>
-            <div style="flex: 1;">
-                <button style="width: auto; background-color: #2908df; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>${Status}</button>
-            </div>
-            <div style="flex: 1;">
-                <button style="width: auto; background-color: #ffffff; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>${ProposalType}</button>
-            </div>
-            <div style="flex: 1;">${Countdown}</div>
-            <div style="flex: 1; display: flex; justify-content: flex-end;">
-                <a href="CurrentVote.html" style="text-decoration: none; color: inherit; outline: none;">
-                <button class="
-                view_more_button" style="background-color: #16ECD3;">View more</button>
-                </a>
-            </div>
-        </div>
+        
         `;
         ProposalQueueList.innerHTML += ProposalHTML;
     }
