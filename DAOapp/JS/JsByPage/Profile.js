3402540fa23af37154686a5489f6e7f81018cb38
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 04:10:04 +0000

    Mar 3, 2024, 11:10 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -4,7 +4,8 @@ async function runCodeWhenLoggedIn() {
         await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
         console.log("Waiting for login");
     }
-
+    
+    CurrentProposalInfo = await GetCurrentProposal();
     GetVotedProposals();
     console.log("Logged in");
 }
