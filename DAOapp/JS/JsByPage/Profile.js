d61271fd32d979a46e3e5dbb4ebda08302024380
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 04:22:13 +0000

    Mar 3, 2024, 11:22 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -61,6 +61,7 @@ async function GetVotedProposals(){
             AmountButton = VoterInfo.VotesLocked;
         } else {
             NextProposalStarted = "";
+            AmountButton = "Claim";
         }
         let ProposalHTML = `
         <div style="display: flex; justify-content: space-between; align-items:center; padding: 16px 20px; border-bottom:1.81818px solid rgb(255, 255, 255); font-size:16px; color:rgb(255, 255, 255); box-sizing: border-box;">
