f7cb27d7b3f38dfac178a96f2a413473e1f430d2
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 02:29:38 +0000

    Mar 3, 2024, 9:29 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -63,7 +63,7 @@ async function GetVotedProposals(){
             <div style="flex: 1;">${TimeLeft}</div>
             <div style="flex: 1; display: flex; justify-content: flex-end;">
                 <a onclick="ClaimInstance(${ID})" style="text-decoration: none; color: inherit; outline: none;">
-                <button class="view_more_button" ${AlreadyClaimed}" style="display: ${IsOver};">Return Votes</button>             
+                <button class="view_more_button" ${AlreadyClaimed} style="display: ${IsOver};">Return Votes</button>             
                 </a>
             </div>
         </div>
