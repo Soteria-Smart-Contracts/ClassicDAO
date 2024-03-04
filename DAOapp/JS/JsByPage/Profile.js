3edb18384808ebb7237c6f00873520f76ce40998
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 03:56:45 +0000

    Mar 3, 2024, 10:56 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -84,7 +84,7 @@ async function ClaimInstance(ProposalID){
     let gas = await DAOvoting.methods.ReturnTokens(ProposalID).estimateGas({from: account});
     await DAOvoting.methods.ReturnTokens(ProposalID).send({from: account, gas: gas});
 
-    alert("Transaction sent, please wait for it to be mined, page will refresh when done");
+    alert("Transaction sent, please wait for it to be mined, page will refresh when done, see balance update on the dashboard");
 
     //refresh the page
     location.reload();
