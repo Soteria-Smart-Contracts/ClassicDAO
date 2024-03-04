521f374d3bf9711395fc00676c96ce4514ffa14b
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 16:00:06 +0000

    Mar 4, 2024, 11:00 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -10,7 +10,6 @@
 // <div style="flex: 1;">
 //     <button style="width: auto; background-color: #ffffff; color: #000; border: 2px solid #000; padding: 0px 10px; border-radius: 10px; font-size: 18px; font-family: GTWalsheim2, regular; cursor: pointer; height: 30px;" disabled>TYPE (SIMPLE OR EROS)</button>
 // </div>
-// <div id="genesiscountdown" style="flex: 1;"></div>
 // <div style="flex: 1; display: flex; justify-content: flex-end;">
 //     <a href="CurrentVote.html" style="text-decoration: none; color: inherit; outline: none;">
 //     <button class="view_more_button" style="background-color: #16ECD3;">View more</button>             
