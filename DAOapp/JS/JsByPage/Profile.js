447038918d2886dd76a7b4160aefb7fd12ce2ce6
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 23:52:36 +0000

    Mar 4, 2024, 6:52 PM

--- DAOapp/JS/JsByPage/Profile.js
+++ DAOapp/JS/JsByPage/Profile.js
@@ -49,7 +49,8 @@ async function GetVotedProposals(){
         VotesLocked = web3.utils.fromWei(CurrentProposalInfo[2].TotalCLDVoted, 'ether');
         VoterVotesLocked = web3.utils.fromWei(VoterInfo.VotesLocked, 'ether');
         Reward = (CLDtoIncentive / VotesLocked * VoterVotesLocked).toFixed(2) + " CLD";
-        //if the proposal is over, recaculate the reward by checking the IncentivePerVote on the 3rd array of the proposal info, and devide it by the votes locked , then multiply it by the users votes locked
+        //if the proposal is over, recaculate the reward by checking the IncentivePerVote on the 3rd array of the proposal info multiplied by the users votes locked
+        if(IsOver){
             ProposalStatus = "Over";
             TimeLeft = "Over";
             IsOver = "flex";
