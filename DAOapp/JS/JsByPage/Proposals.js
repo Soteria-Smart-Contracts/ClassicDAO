76893c3e2a9d001f72ff01e458af385b9e09c737
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 15:23:24 +0000

    Mar 4, 2024, 10:23 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -21,6 +21,28 @@
 
 //Load the proposal queue and the proposal details for each proposal in the queue. Here are the details needed: ID, Status, ProposalInfo.ProposalType, VotingInstanceID, VotingInstance.Status, VotingInstance.CLDtoIncentive
 async function LoadProposals() {
-
-    
+    let ProposalQueue = await DAOvoting.methods.ProposalQueue().call();
+    let ProposalQueueList = document.getElementById("ProposalQueueList");
+    ProposalQueueList.innerHTML = "";
+    for (let i = 0; i < ProposalQueue.length; i++) {
+        let ProposalID = ProposalQueue[i];
+        let ProposalInfo = await DAOvoting.methods.ProposalInfo(ProposalID).call();
+        let VotingInstance = await DAOvoting.methods.VotingInstances(ProposalID).call();
+        let ProposalType = ProposalInfo.ProposalType;
+        let Status = VotingInstance.Status;
+        let CLDtoIncentive = web3.utils.fromWei(VotingInstance.CLDtoIncentive, 'ether');
+        let Proposal = document.createElement("div");
+        Proposal.classList.add("Proposal");
+        Proposal.innerHTML = `
+        <div class="ProposalID">${ProposalID}</div>
+        <div class="ProposalType">${ProposalType}</div>
+        <div class="Status">${Status}</div>
+        <div class="CLDtoIncentive">${CLDtoIncentive}</div>
+        <div class="VotingInstanceID">${VotingInstance.VotingInstanceID}</div>
+        <div class="VotingInstanceStatus">${Status}</div>
+        <button class="VoteButton" onclick="Vote(${ProposalID})">Vote</button>
+        `;
+        ProposalQueueList.appendChild(Proposal);
+    }
+}
     
