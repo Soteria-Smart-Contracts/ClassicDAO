73ce99bc3cacb2e2c2677932b3f4a424f5603b78
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 04:15:52 +0000

    Mar 3, 2024, 11:15 PM

--- DAOapp/JS/Web3base.js
+++ DAOapp/JS/Web3base.js
@@ -17,7 +17,7 @@ setInterval(async function(){
     if (LoggedIn == true){
         accountarray = await web3.eth.getAccounts();
         if(accountarray[0] != account){
-            location.reload();
+            //location.reload();
         }
     }
 }, 1000);
