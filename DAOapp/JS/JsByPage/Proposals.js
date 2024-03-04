b17c8c0e5e84bac370a459755e07b15912d695e7
Darius Tanz <scratchticketseb@gmail.com>
2024-03-04 16:10:43 +0000

    Mar 4, 2024, 11:10 AM

--- DAOapp/JS/JsByPage/Proposals.js
+++ DAOapp/JS/JsByPage/Proposals.js
@@ -59,6 +59,7 @@ async function LoadProposals() {
         let VotingInstance = await DAOvoting.methods.VotingInstances(ProposalQueue[i]).call();
         let ProposalInfo = await DAOcore.methods.ProposalInfos(ProposalQueue[i]).call();
         let Status = "Queued Up";
+        //if the st
         let ProposalType = "Simple";
         if (ProposalInfo.ProposalType == 1) {
             ProposalType = "Eros";
