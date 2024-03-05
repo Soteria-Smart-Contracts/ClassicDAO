2c31a8cadb2645e3384f418424aa92d42e106388
Darius Tanz <scratchticketseb@gmail.com>
2024-03-05 00:29:06 +0000

    Mar 4, 2024, 7:29 PM

--- DAOapp/JS/JsByPage/Treasury.js
+++ DAOapp/JS/JsByPage/Treasury.js
@@ -1,1 +1,13 @@
 //TreasuryCLD Balance and TreasuryETC balance set ids
+async function runCodeWhenLoggedIn() {
+    while (!LoggedIn) {
+        await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second
+        console.log("Waiting for login");
+    }
+
+    CurrentProposalInfo = await GetCurrentProposal();
+    GetVotedProposals();
+    console.log("Logged in");
+}
+
+runCodeWhenLoggedIn();
