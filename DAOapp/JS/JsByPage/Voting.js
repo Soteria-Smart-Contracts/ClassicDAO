3353a0684cd9665080220c57e7fd2faa4a1c2b1c
Darius Tanz <scratchticketseb@gmail.com>
2024-03-15 15:37:51 +0000

    Mar 15, 2024, 11:37 AM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -197,7 +197,7 @@ async function SubmitVote(){
     //transaction send, alert the user that the vote has been submitted, with a link to the transaction on blockscout, 
     //then reload the page
     document.getElementById('voteoverlay').style.display = 'none';
-    alert("Vote Submitted, view the transaction on Blockscout: https://etc.blockscout.com/tx/" + transactionHash);
+    alert(Vote Submitted, view the transaction on Blockscout: https://etc.blockscout.com/tx/" + transactionHash);
     while (true) {
         await new Promise(resolve => setTimeout(resolve, 1000));
         if ((await DAOvoting.methods.VoterInfo(CurrentProposalInfo[2].ProposalID, account).call()).CLDReturned > 0) {
