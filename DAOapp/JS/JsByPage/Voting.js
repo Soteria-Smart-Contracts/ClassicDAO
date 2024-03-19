2e378a37d65c766d629c6d44ee9f51206dccc2e1
Darius Tanz <scratchticketseb@gmail.com>
2024-03-19 14:53:02 +0000

    Mar 19, 2024, 10:53 AM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -31,6 +31,7 @@ async function LoadDashboard() {
 
     // for every (<>) in the description text, replace it with a line break
     document.getElementById("desc").innerHTML = document.getElementById("desc").innerHTML.replace((/&lt;&gt;/g), "<br><br>");
+    //remove all bra
     console.log(document.getElementById("desc").innerHTML);
 
     //calculate the total incentive per vote, the total incentive per vote is the total incentive divided by the total votes, total incentive is CLDtoIncetive on  current proposal info 3rd array, total votes is TotalCLDVoted in 3rd array of CurrentProposalInfo
