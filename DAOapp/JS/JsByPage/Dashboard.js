d3392e06ace145d315d5a750bfafc2fcfa6834f0
Darius Tanz <scratchticketseb@gmail.com>
2024-03-24 16:22:10 +0000

    Mar 24, 2024, 12:22 PM

--- DAOapp/JS/JsByPage/Dashboard.js
+++ DAOapp/JS/JsByPage/Dashboard.js
@@ -13,9 +13,11 @@ async function LoadDashboard() {
     //HeaderProposer id
     document.getElementById("HeaderProposer").innerText = (CurrentProposalInfo[0].Proposer).substring(0, 7) + '...' + (CurrentProposalInfo[0].Proposer).substring(account.length - 3, CurrentProposalInfo[0].Proposer);
     //HeaderMemo found as memo in 2nd array of CurrentProposalInfo
-    document.getElementById("ProposalMemo").innerText = (CurrentProposalInfo[1].Memo).split("//")[0];
-    document.getElementById("desc").innerText = (CurrentProposalInfo[1].Memo).split("//")[1];
-    document.getElementById("desc").innerText = document.getElementById("desc").innerHTML.split((/[(]&lt;&gt;[)]/g))[0];
+    document.getElementById("ProposalMemo").innerText = (CurrentProposalInfo[1].Memo).split("!!")[0];
+    document.getElementById("desc").innerText = (CurrentProposalInfo[1].Memo).split("!!")[1];
+
+    // for every (<>) in the description text, replace it with a line break
+    document.getElementById("desc").innerHTML = document.getElementById("desc").innerHTML.replace((/[(]&lt;&gt;[)]/g), "<br><br>");
 
     //HeaderTimeLeft id found in 3rd array of CurrentProposalInfo as VoteEnds in unix time, convert to format as such (2 days 6 hours)
     document.getElementById("HeaderTimeLeft").innerText = timeLeft(CurrentProposalInfo[2].VoteEnds);
