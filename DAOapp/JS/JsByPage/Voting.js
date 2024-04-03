af62d2893620497d748253b7c8166f97962f6a6e
Darius Tanz <scratchticketseb@gmail.com>
2024-04-03 17:39:18 +0000

    Apr 3, 2024, 1:39 PM

--- DAOapp/JS/JsByPage/Voting.js
+++ DAOapp/JS/JsByPage/Voting.js
@@ -48,7 +48,7 @@ async function LoadDashboard() {
         document.getElementById("votebuttons").style.display = "none";
         document.getElementById("votebuttonstext").innerText = "Proposal is in Debate Period";
     }
-    //check if there are any proposals in the queue, if there are not, display the Begin Vote button for all users
+    //check if there are any proposals in the queue
     //else if the vote end time is less than the current time, the vote has ended, so disable the vote buttons at votebuttons id and write Proposal has Ended in id votebuttonstext
     else if (CurrentProposalInfo[2].VoteEnds < Math.floor(Date.now() / 1000)) {
         document.getElementById("votebuttons").style.display = "none";
